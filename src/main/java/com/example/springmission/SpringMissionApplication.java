92deb59762ee59b016d0f1b86a13255eb5afc44c
Jaewook Choi <eari100@naver.com>
2023-08-26 20:33:22 +0900

    Initial commit

---
+++ src/main/java/com/example/springmission/SpringMissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springmission/SpringMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
